419b24ce16ce5fb00f1763419d9e46ca1e033211
imrriederich-bit <imr.riederich@gmail.com>
2026-04-09 21:47:05 +0300

    Add files via upload

---
+++ src/constants/screens.js
@@ -0,0 +1,16 @@
+export const SCREENS = {
+  dashboard: "dashboard",
+  tax: "tax",
+  mortgage: "mortgage",
+  electricity: "electricity",
+  insurance: "insurance",
+  premium: "premium",
+  legal: "legal",
+};
+
+export const MODULE_SCREENS = [
+  SCREENS.tax,
+  SCREENS.mortgage,
+  SCREENS.electricity,
+  SCREENS.insurance,
+];
